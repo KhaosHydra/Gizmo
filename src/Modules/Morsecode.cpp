7252d1c226cfb33c51ec873f0f554806fa576645
KhaosHydra <krystalcheung14@gmail.com>
2024-11-30 18:25:26 +0000

    Random Blink Morse code generator

--- src/Modules/Morsecode.cpp
+++ src/Modules/Morsecode.cpp
@@ -1,15 +1,22 @@
-// /*############################################################################
-//   #                 This code is testinng the rotary encoder                 #
-//   ############################################################################*/
+// #include <Arduino.h>
+// #include <Wire.h>
+// #include <TM1637Display.h>
 
-// // Libraries
-// #include <Arduino.h> // Arduino library
-// #include <Wire.h> // I2C library
+// // Module connection pins (Digital Pins)
+// #define SevSegCLK 22 // CLK of the display
+// #define SevSegDIO 23 // DIO of the display
+// #define rotaryEncoderA 35   // CLK of the rotary encoder
+// #define rotaryEncoderB 34   // DT of the rotary encoder
+// #define RotaryEncoderButton 32  // Button of the rotary encoder
 
-// // Pins
-// #define rotaryEncoderA 35
-// #define rotaryEncoderB 34
-// #define RotaryEncoderButton 32
+// // Display for "no"
+// const uint8_t SEG_NO[] = {
+//   SEG_C | SEG_E | SEG_G,                          // Displays n
+//   SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F   // Displays o
+// };
+
+// // Create a display object of type TM1637Display
+// TM1637Display display(SevSegCLK, SevSegDIO);
 
 // // Variables
 // int number = 0;
@@ -17,27 +24,35 @@
 // int previousStateRotaryEncoderB = HIGH;
 // int previousStateRotaryEncoderButton = HIGH;
 
-// // Functions
-
-// void setup() {
+// void setup()
+// {
 //   // put your setup code here, to run once:
 //   Serial.begin(9600);
 
+//   // Set the rotary encoder pins as inputs
 //   pinMode(rotaryEncoderA, INPUT);
 //   pinMode(rotaryEncoderB, INPUT);
 //   pinMode(RotaryEncoderButton, INPUT);
 // }
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
+// void loop()
+// {
+//   // Initialize the display
+//   int k;
+//   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+//   uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+//   display.setBrightness(0x0f);
+
+//   // seeing position of rotary encoder
 //   int rotaryA = digitalRead(rotaryEncoderA);
 //   int rotaryB = digitalRead(rotaryEncoderB);
 //   int buttonState = digitalRead(RotaryEncoderButton);
 
-//   if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
+//   // change display number
+//     if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
 //     delay(50); // debounce delay
 //     if (rotaryB == LOW) {
-//       // Turn right function
+//       // Turn right function increases number on display
 //       number++;
 //       Serial.println(number);
 //     }
@@ -47,15 +62,21 @@
 //     Serial.println(number);
 //     }
 //   }
-//   previousStateRotaryEncoderA = rotaryA;
+//   previousStateRotaryEncoderA = rotaryA;  // save the current state as the last state, for next time through the loop
 
-
-//   // Checks debounced button press
+//     // Checks debounced button press
 //   if (previousStateRotaryEncoderButton == HIGH && buttonState == LOW) {
 //     delay(50); // debounce delay
 //     if (digitalRead(RotaryEncoderButton) == LOW) { // check again after delay
 //     // do a function
-//       Serial.println("Button pressed");
+//       display.clear();
+//       // Serial.println("Button pressed");
+//       display.setSegments(SEG_NO, 2, 1);  // Display "no" in the centre
+//       delay(5000);
 //     }
 //   }
-//   previousStateRotaryEncoderButton = buttonState;
+//   previousStateRotaryEncoderButton = buttonState;   // save the current state as the last state, for next time through the loop
+
+//   display.showNumberDec(number, false); // Display
+
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,101 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <TM1637Display.h>
+#include <map>
 
 // Module connection pins (Digital Pins)
-#define SevSegCLK 22
-#define SevSegDIO 23
-#define rotaryEncoderA 35
-#define rotaryEncoderB 34
-#define RotaryEncoderButton 32
+#define MorseCode_LED 33 // LED for Morse code
 
-const uint8_t SEG_NO[] = {
-  SEG_C | SEG_E | SEG_G,                          // Displays n
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F   // Displays o
-};
+// Global Variables
+int DotDuration = 1000;
+int DashDuration = 3000;
+int betweenElements = 1000;
+int betweenNumberDuration = 3000;
 
-TM1637Display display(SevSegCLK, SevSegDIO);
+// Secret Morse code Passcode
+int numberPasscode;
 
-// Variables
-int number = 0;
-int previousStateRotaryEncoderA = HIGH;
-int previousStateRotaryEncoderB = HIGH;
-int previousStateRotaryEncoderButton = HIGH;
-
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  pinMode(rotaryEncoderA, INPUT);
-  pinMode(rotaryEncoderB, INPUT);
-  pinMode(RotaryEncoderButton, INPUT);
+// Mapping morse code to digits
+String digitToMorse(char digit) {
+    switch (digit) {
+        case '0': return "-----";
+        case '1': return ".----";
+        case '2': return "..---";
+        case '3': return "...--";
+        case '4': return "....-";
+        case '5': return ".....";
+        case '6': return "-....";
+        case '7': return "--...";
+        case '8': return "---..";
+        case '9': return "----.";
+        default: return ""; // Invalid input
+    }
 }
 
+// Function to convert number to Morse code
+String numberToMorse(int number) {
+    String morseCode = "";
+    String numberStr = String(number); // Convert number to string
+    for (size_t i = 0; i < numberStr.length(); i++) {
+        morseCode += digitToMorse(numberStr[i]);
+        if (i < numberStr.length() - 1) {
+            morseCode += " "; // Add space between Morse codes
+        }
+    }
+    return morseCode;
+}
+
+// Function to blink the LED according to Morse code
+void blinkLEDMorseCode(const String &morseCode)
+{
+  for (char c : morseCode)
+  {
+    // Dot Duration
+    if (c == '.')
+    {
+      digitalWrite(MorseCode_LED, HIGH);
+      delay(DotDuration);
+      digitalWrite(MorseCode_LED, LOW);
+      delay(betweenElements);
+    }
+    // Dash Duration
+    else if (c == '-')
+    {
+      digitalWrite(MorseCode_LED, HIGH);
+      delay(DashDuration);
+      digitalWrite(MorseCode_LED, LOW);
+      delay(betweenElements);
+    }
+    // Gap Duration
+    else
+    {
+      delay(betweenNumberDuration);
+    }
+  }
+}
+
+// Function to setup the code
+void setup()
+{
+  // Start the serial communication
+  Serial.begin(9600);
+
+  // Set the LED pin as output
+  pinMode(MorseCode_LED, OUTPUT);
+
+  // Generate random number
+  numberPasscode = esp_random() % 10000;
+}
+
+// Function to loop the code
 void loop()
 {
-  // Initialize the display
-  int k;
-  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
-  display.setBrightness(0x0f);
+  // Convert number to Morse code
+  String morseCode = numberToMorse(numberPasscode);
 
-  // seeing position of rotary encoder
-  int rotaryA = digitalRead(rotaryEncoderA);
-  int rotaryB = digitalRead(rotaryEncoderB);
-  int buttonState = digitalRead(RotaryEncoderButton);
-
-  // change display number
-    if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
-    delay(50); // debounce delay
-    if (rotaryB == LOW) {
-      // Turn right function
-      number++;
-      Serial.println(number);
-    }
-    else{
-      // Turn left function
-      number--;
-    Serial.println(number);
-    }
+  // Blink the LED according to morse code
+  while (true){
+    blinkLEDMorseCode(morseCode);
+    delay(7000);
   }
-  previousStateRotaryEncoderA = rotaryA;  // save the current state as the last state, for next time through the loop
-
-    // Checks debounced button press
-  if (previousStateRotaryEncoderButton == HIGH && buttonState == LOW) {
-    delay(50); // debounce delay
-    if (digitalRead(RotaryEncoderButton) == LOW) { // check again after delay
-    // do a function
-      display.clear();
-      // Serial.println("Button pressed");
-      display.setSegments(SEG_NO, 2, 1);  // Display "no" in the centre
-      delay(5000);
-    }
-  }
-  previousStateRotaryEncoderButton = buttonState;   // save the current state as the last state, for next time through the loop
-
-  display.showNumberDec(number, false); // Display
 
 }
