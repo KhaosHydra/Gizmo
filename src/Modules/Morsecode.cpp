9f8bc77f58ca42116492eca11ae02f92067b6f7f
KhaosHydra <krystalcheung14@gmail.com>
2024-11-30 13:58:35 +0000

    Rotary encoder and display

--- src/Modules/Morsecode.cpp
+++ src/Modules/Morsecode.cpp
@@ -1,34 +1,61 @@
-// #include <Arduino.h>
-// #include "SevSegShift.h"
+// /*############################################################################
+//   #                 This code is testinng the rotary encoder                 #
+//   ############################################################################*/
 
-// // Define the pins for the shift registers
-// #define DATA_PIN  23  // DS (Data Serial)
-// #define LATCH_PIN 22  // ST_CP (Storage Register Clock Pin)
-// #define CLOCK_PIN 21  // SH_CP (Shift Register Clock Pin)
+// // Libraries
+// #include <Arduino.h> // Arduino library
+// #include <Wire.h> // I2C library
 
-// // Create a SevSegShift object
-// SevSegShift sevseg(DATA_PIN, CLOCK_PIN, LATCH_PIN);
+// // Pins
+// #define rotaryEncoderA 35
+// #define rotaryEncoderB 34
+// #define RotaryEncoderButton 32
+
+// // Variables
+// int number = 0;
+// int previousStateRotaryEncoderA = HIGH;
+// int previousStateRotaryEncoderB = HIGH;
+// int previousStateRotaryEncoderButton = HIGH;
+
+// // Functions
 
 // void setup() {
-//   // Initialize Serial Monitor for debugging
+//   // put your setup code here, to run once:
 //   Serial.begin(9600);
 
-//   // Initialize the SevSegShift object
-//   byte numDigits = 4;
-//   byte digitPins[] = {8+1, 8+2, 8+3, 8+4}; // of ShiftRegister(s) | 8+x (2nd Register)
-//   byte segmentPins[] = {0, 1, 2, 3, 4, 5, 6}; // of Shiftregister(s) | 8+x (2nd Register)
-//   bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
-//   byte hardwareConfig = COMMON_ANODE; // Your display type
-//   bool updateWithDelays = false; // Default 'false' is recommended
-//   bool leadingZeros = false; // Use 'true' if you want leading zeros
-//   bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected
-
-//   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, updateWithDelays, leadingZeros, disableDecPoint);
-//   sevseg.setBrightness(100); // Set brightness (0-100)
+//   pinMode(rotaryEncoderA, INPUT);
+//   pinMode(rotaryEncoderB, INPUT);
+//   pinMode(RotaryEncoderButton, INPUT);
 // }
 
 // void loop() {
-//   sevseg.setNumber(1234);
-//   sevseg.refreshDisplay(); // Must run repeatedly to refresh the display
-//   delay(1000); // Update the display every second
-// }
+//   // put your main code here, to run repeatedly:
+//   int rotaryA = digitalRead(rotaryEncoderA);
+//   int rotaryB = digitalRead(rotaryEncoderB);
+//   int buttonState = digitalRead(RotaryEncoderButton);
+
+//   if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
+//     delay(50); // debounce delay
+//     if (rotaryB == LOW) {
+//       // Turn right function
+//       number++;
+//       Serial.println(number);
+//     }
+//     else{
+//       // Turn left function
+//       number--;
+//     Serial.println(number);
+//     }
+//   }
+//   previousStateRotaryEncoderA = rotaryA;
+
+
+//   // Checks debounced button press
+//   if (previousStateRotaryEncoderButton == HIGH && buttonState == LOW) {
+//     delay(50); // debounce delay
+//     if (digitalRead(RotaryEncoderButton) == LOW) { // check again after delay
+//     // do a function
+//       Serial.println("Button pressed");
+//     }
+//   }
+//   previousStateRotaryEncoderButton = buttonState;
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
-/*############################################################################
-  #                 This code is testinng the rotary encoder                 #
-  ############################################################################*/
+#include <Arduino.h>
+#include <Wire.h>
+#include <TM1637Display.h>
 
-// Libraries
-#include <Arduino.h> // Arduino library
-#include <Wire.h> // I2C library
-
-// Pins
+// Module connection pins (Digital Pins)
+#define SevSegCLK 22
+#define SevSegDIO 23
 #define rotaryEncoderA 35
 #define rotaryEncoderB 34
 #define RotaryEncoderButton 32
+
+const uint8_t SEG_NO[] = {
+  SEG_C | SEG_E | SEG_G,                          // Displays n
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F   // Displays o
+};
+
+TM1637Display display(SevSegCLK, SevSegDIO);
 
 // Variables
 int number = 0;
@@ -17,9 +22,8 @@ int previousStateRotaryEncoderA = HIGH;
 int previousStateRotaryEncoderB = HIGH;
 int previousStateRotaryEncoderButton = HIGH;
 
-// Functions
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
 
@@ -28,13 +32,21 @@ void setup() {
   pinMode(RotaryEncoderButton, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Initialize the display
+  int k;
+  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+  display.setBrightness(0x0f);
+
+  // seeing position of rotary encoder
   int rotaryA = digitalRead(rotaryEncoderA);
   int rotaryB = digitalRead(rotaryEncoderB);
   int buttonState = digitalRead(RotaryEncoderButton);
 
-  if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
+  // change display number
+    if (previousStateRotaryEncoderA == HIGH && rotaryA == LOW) {
     delay(50); // debounce delay
     if (rotaryB == LOW) {
       // Turn right function
@@ -47,17 +59,21 @@ void loop() {
     Serial.println(number);
     }
   }
-  previousStateRotaryEncoderA = rotaryA;
+  previousStateRotaryEncoderA = rotaryA;  // save the current state as the last state, for next time through the loop
 
-
-  // Checks debounced button press
+    // Checks debounced button press
   if (previousStateRotaryEncoderButton == HIGH && buttonState == LOW) {
     delay(50); // debounce delay
     if (digitalRead(RotaryEncoderButton) == LOW) { // check again after delay
     // do a function
-      Serial.println("Button pressed");
+      display.clear();
+      // Serial.println("Button pressed");
+      display.setSegments(SEG_NO, 2, 1);  // Display "no" in the centre
+      delay(5000);
     }
   }
-  previousStateRotaryEncoderButton = buttonState;
+  previousStateRotaryEncoderButton = buttonState;   // save the current state as the last state, for next time through the loop
+
+  display.showNumberDec(number, false); // Display
 
 }
